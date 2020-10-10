8303e80fbb4a5ce5f87d31a916df2a6d5bb884ff
Jodhandeep Singh <49287012+jdsingh122918@users.noreply.github.com>
2020-10-10 10:05:40 -0500

    Added grep module - part 1

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_name)?;
+
+    println!("Here is the query: {}", config.query);
+    println!("\n{}", contents);
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_name: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Ok(Config { query, file_name })
+    }
+}
