005aadb08502ebc9a91aafb57c8c6ddc35e84f17
Jodhandeep Singh <49287012+jdsingh122918@users.noreply.github.com>
2020-10-15 17:29:28 -0500

    Added unit test and improved logic.

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,31 +1,33 @@
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
 struct Cacher<T>
-    where
-        T: Fn(u32) -> u32,
+where
+    T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: Option<u32>,
+    value: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
-    where
-        T: Fn(u32) -> u32,
+where
+    T: Fn(u32) -> u32,
 {
     fn new(calculation: T) -> Cacher<T> {
+        let result_hashmap: HashMap<u32, u32> = HashMap::new();
         Cacher {
             calculation,
-            value: None,
+            value: result_hashmap,
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
+        match self.value.get(&arg) {
+            Some(&number) => number,
             None => {
                 let v = (self.calculation)(arg);
-                self.value = Some(v);
+                self.value.insert(arg, v);
                 v
             }
         }
@@ -40,14 +42,14 @@ fn generate_workout(intensity: u32, random_number: u32) {
     });
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result.value(intensity));
-        println!("Next, do {} situps!", expensive_result.value(intensity));
+        println!("Today, do {:?} pushups!", expensive_result.value(intensity));
+        println!("Next, do {:?} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
             println!(
-                "Today, run for {} minutes!",
+                "Today, run for {:?} minutes!",
                 expensive_result.value(intensity)
             );
         }
@@ -59,4 +61,19 @@ fn main() {
     let simulated_random_number = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(v1, 1);
+        assert_eq!(v2, 2);
+    }
 }
